19a0335308eca969515c609a4c1232176161bec8
JVPH <gyrospizzeria@protonmail.com>
2022-10-28 13:14:41 -0500

    add build folder

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express()
 
 app.use(cors());
+app.use(express.static('build'));
 
 let notes = [
   {
@@ -87,7 +88,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
